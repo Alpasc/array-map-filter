0f25c2f33c304b49a95c03090e0d0b62ff25fcde
Alpasc <alinepascal@gmail.com>
2020-04-01 10:10:33 +0200

    resolution filter 1

--- filter/ex1.js
+++ filter/ex1.js
@@ -1,4 +1,4 @@
-/* Array.prototype.filter - Exercise 1
+/*Array.prototype.filter - Exercise 1
 
 * Write the CONTENTS of the `getPositiveNumbers` function (don't rename it!)
 * It receives an array of numbers as an argument
@@ -9,12 +9,16 @@ Sample numbers array, i.e. INPUT:
   [7, -4, 2, 0, -10, 3, 11]
 
 Expected OUTPUT for this sample:
-  [7, 2, 0, 3, 11]
+  [7, 2, 0, 3, 11]*/
 
-*/
+
 
 function getPositiveNumbers(numbers) {
-}
+  return numbers.filter((number) => number>=0);
+};
+
+ 
+
 
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
@@ -22,7 +26,7 @@ function getPositiveNumbers(numbers) {
 // Instead of running `npm test`, you can then use `node filter/ex1`
 // (you still need to run `npm test` to make sure you did right).
 
-// console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
+//console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
 
 // DON'T TOUCH THIS!
 module.exports = getPositiveNumbers;
--- filter/ex2.js
+++ filter/ex2.js
@@ -24,9 +24,15 @@ Expected OUTPUT for this sample:
   ['Bran Stark', 'Arya Stark', 'Sansa Stark']
 
 */
+const familyStark = [];
 
 function keepStarks(names) {
+  for (let index = 0; index < names.length; index++){
+    if (name.endWith('Stark')){
+      familyStark.push(name);
+    }
+  }
 }
-
+return keepStarks
 // DON'T TOUCH THIS!
 module.exports = keepStarks;
